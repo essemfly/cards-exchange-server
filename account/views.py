0620076173a8e66440bc7acb6b8d66fef1b768bd
essemfly <essemfly@gmail.com>
2017-08-08 18:12:29 +0900

    Add user detail rest api

---
+++ account/views.py
@@ -0,0 +1,31 @@
+from django.http import Http404
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+from account.serializers import UserSerializer
+
+
+class UserDetail(APIView):
+    def get_object(selfself, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request, pk, format=None):
+        pass

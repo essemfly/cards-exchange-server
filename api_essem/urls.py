78ac88821aefdd7f29ffec58d510971d71f12a0b
essemfly <essemfly@gmail.com>
2017-08-28 20:30:41 +0900

    Add swagger UI in main page and renew login api

--- api_essem/urls.py
+++ api_essem/urls.py
@@ -17,8 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Essem API')
 
 urlpatterns = [
+    url(r'^$', schema_view),
     url(r'^', include('rest_auth.urls')),
     url(r'^dogs/', include('dogs.urls')),
     url(r'^admin/', admin.site.urls),

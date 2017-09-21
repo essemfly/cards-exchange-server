33e0376f12c20522f857b536e524f88cdee90f49
essemfly <essemfly@gmail.com>
2017-09-21 17:06:07 +0900

    Make request object and end with basic function

--- cards/admin.py
+++ cards/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (
-    Card, CardGroup, CardMember, CardType, CardVersion
+    Card, CardGroup, CardMember, CardType, CardVersion, CardRequest,
 )
 
 
@@ -29,8 +29,15 @@ class CardAdmin(admin.ModelAdmin):
     list_display = ['group', 'version', 'member', 'type']
 
 
+class CardRequestAdmin(admin.ModelAdmin):
+    model = CardRequest
+    list_display = ['requester', 'matcher', 'have_card', 'want_card',
+                    'created_date', 'matched_date', 'status']
+
+
 admin.site.register(CardGroup, CardGroupAdmin)
 admin.site.register(CardMember, CardMemberAdmin)
 admin.site.register(CardVersion, CardVersionAdmin)
 admin.site.register(CardType, CardTypeAdmin)
 admin.site.register(Card, CardAdmin)
+admin.site.register(CardRequest, CardRequestAdmin)
--- cards/serializers.py
+++ cards/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from cards.models import Card, CardRequest
+from django.contrib.auth.models import User
 
 
 class CardSerializer(serializers.ModelSerializer):
@@ -14,10 +15,12 @@ class CardSerializer(serializers.ModelSerializer):
 
 
 class CardRequestSerializer(serializers.ModelSerializer):
-    requester = serializers.PrimaryKeyRelatedField(read_only=True)
-    matcher = serializers.PrimaryKeyRelatedField(read_only=True)
+    requester = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    matcher = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
     have_card = serializers.StringRelatedField()
+    want_card = serializers.StringRelatedField()
 
     class Meta:
         model = CardRequest
-        fields = ('requester', 'matcher', 'have_card')
+        fields = ('requester', 'matcher', 'have_card', 'want_card',
+                  'created_date', 'updated_date', 'matched_date', 'status')
